d1cb29e7bdc21fd29be0058eab6f6b133ac4524d
Sumit Anand Jha <125876713+sumitanandjha@users.noreply.github.com>
2024-08-24 12:14:57 +0530

    one more

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SendInput from './SendInput';
 import Messages from './Messages';
 import { useSelector } from 'react-redux';
-import classNames from 'classnames';
 
 const MessageContainer = () => {
     const { selectedUser, authUser, onlineUsers } = useSelector((store) => store.user);
@@ -14,7 +13,7 @@ const MessageContainer = () => {
             {selectedUser ? (
                 <div className='md:min-w-[550px] flex flex-col'>
                     <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-                        <div className={classNames('avatar', { 'online': isOnline })}>
+                        <div className={`avatar ${isOnline ? 'online' : ''}`}>
                             <div className='w-12 rounded-full'>
                                 <img
                                     src={selectedUser?.profilePhoto || '/default-profile.png'}
