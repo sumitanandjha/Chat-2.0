4f4bc4c31c49f6e355c9c98e7a0e85718923620b
Sumit Anand Jha <125876713+sumitanandjha@users.noreply.github.com>
2024-08-24 12:03:18 +0530

    next commit

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { setOtherUsers } from '../redux/userSlice';
 import { BaseUrl } from '../constant/Base';
 
@@ -8,20 +8,29 @@ const useGetOtherUsers = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
+        let isMounted = true; // To track if the component is mounted
+
         const fetchOtherUsers = async () => {
             try {
-                axios.defaults.withCredentials = true;
+                axios.defaults.withCredentials = true; // Ensure this is needed; otherwise, set it globally
                 const res = await axios.get(`${BaseUrl}/api/v1/user`);
-                // store
-                console.log("other users -> ",res);
-                dispatch(setOtherUsers(res.data));
+                
+                if (isMounted) {
+                    // Store data only if the component is still mounted
+                    console.log("other users -> ", res);
+                    dispatch(setOtherUsers(res.data));
+                }
             } catch (error) {
-                console.log(error);
+                console.error('Error fetching other users:', error); // Improved error logging
             }
-        }
+        };
+
         fetchOtherUsers();
-    }, [])
 
-}
+        return () => {
+            isMounted = false; // Cleanup function to set isMounted to false on unmount
+        };
+    }, [dispatch]); // Add dispatch to dependency array to avoid lint warnings
+};
 
-export default useGetOtherUsers
+export default useGetOtherUsers;
