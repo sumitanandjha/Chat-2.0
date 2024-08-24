bb82e873e7ce5bdb3c54407a3af179ce7056f300
Sumit Anand Jha <125876713+sumitanandjha@users.noreply.github.com>
2024-08-24 11:38:11 +0530

    one more commit

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use(cookieParser());
 
-const corsOption = {
-    origin: ['http://localhost:3000', 'https://chat-2-0-1.onrender.com'], // Allowed origins
-    credentials: true, // Allows cookies and other credentials to be sent
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    optionsSuccessStatus: 204, // Status code for successful OPTIONS requests
-    preflightContinue: false, // Determines if the middleware should move to the next middleware after handling OPTIONS requests
-    maxAge: 86400 // Max age for the preflight response in seconds (24 hours)
-};
+// const corsOption = {
+//     origin: ['http://localhost:3000', 'https://chat-2-0-1.onrender.com'], // Allowed origins
+//     credentials: true, // Allows cookies and other credentials to be sent
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+//     optionsSuccessStatus: 204, // Status code for successful OPTIONS requests
+//     preflightContinue: false, // Determines if the middleware should move to the next middleware after handling OPTIONS requests
+//     maxAge: 86400 // Max age for the preflight response in seconds (24 hours)
+// };
 
-app.use(cors(corsOption)); 
+app.use(cors()); 
 
 // Serve static files from the 'frontend/build' directory
 // app.use(express.static(path.join(__dirname, '..', 'frontend', 'build'), {
